dfdd99b06c3ca762619d522b7bec9f1f25a55360
Kyle Kelly <kkelly@zappos.com>
2014-08-13 00:48:22 -0700

    Variables

---
+++ Variables/YourSchedule.java
@@ -0,0 +1,24 @@
+public class YourSchedule
+{
+	public static void main( String[] args )
+	{
+		String class1 = "Java 101", class2 = "HTML5", class3 = "PHP",
+				class4 = "Biotechnology", class5 = "Database Foundations", 
+				class6 = "AP CSS3", class7 = "JavaScript 101",
+				class8 = "Business Computer Information Systems";
+		String teacher1 = "Ms. Jippy", teacher2 = "Mrs. Pippy", teacher3 = "Mr. TeamTreehouse",
+				teacher4 = "Ms. iPhone", teacher5 = "Ms. MattRyan", teacher6 = "Mrs. BoyzInDaHood",
+				teacher7 = "Ms. AtlantaFalcons", teacher8 = "Mr. VH1";
+		
+		System.out.println( "+----------------------------------------------+" );
+		System.out.println( "| 1 | " + class1 + " | " + teacher1 + " |" );
+		System.out.println( "| 2 | " + class2 + " | " + teacher2 + " |" );
+		System.out.println( "| 3 | " + class3 + " | " + teacher3 + " |" );
+		System.out.println( "| 4 | " + class4 + " | " + teacher4 + " |" );
+		System.out.println( "| 5 | " + class5 + " | " + teacher5 + " |" );
+		System.out.println( "| 6 | " + class6 + " | " + teacher6 + " |" );
+		System.out.println( "| 7 | " + class7 + " | " + teacher7 + " |" );
+		System.out.println( "| 8 | " + class8 + " | " + teacher8 + " |" );	
+		
+	}
+}
