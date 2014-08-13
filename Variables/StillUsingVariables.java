58949f8209ddd695650a3b840b0391ed301c6d8e
Kyle Kelly <kkelly@zappos.com>
2014-08-13 00:19:38 -0700

    Variables

---
+++ Variables/StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables
+{
+	public static void main( String[] args )
+	{
+		String full_name = "Kyle Kelly";
+		int year = 2014;
+		
+		System.out.println( "My name is " + full_name + " and I'll graduate in " + year + "." );
+	}
+}
