d51dc2055140ec4d8eda1b79b1ea5cdde03b2ba2
Kyle Kelly <kkelly@zappos.com>
2014-08-12 23:04:33 -0700

    6 Basics and Printing

---
+++ Tasks/YourInitials.java
@@ -0,0 +1,15 @@
+public class YourInitials
+{
+	public static void main( String[] args )
+	{
+		System.out.println( " For the name Kyle Ryan Kelly..." );
+		System.out.println();
+		System.out.println( "K   K  RRRRR   K   K" ); // 1
+		System.out.println( "K  K   R   R   K  K " ); // 2
+		System.out.println( "K K    R   R   K K  " ); // 3
+		System.out.println( "KK     RRRR    KK   " ); // 4
+		System.out.println( "K K    R   R   K K  " ); // 5
+		System.out.println( "K  K   R    R  K  K " ); // 6
+		System.out.println( "K   K  R    R  K   K" ); // 7
+	}
+}
