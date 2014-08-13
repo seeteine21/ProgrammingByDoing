1b3912cf3b94a21718202be66f29631a3c297d33
Kyle Kelly <kkelly@zappos.com>
2014-08-13 00:14:29 -0700

    Variables

---
+++ Variables/UsingVariables.java
@@ -0,0 +1,13 @@
+public class UsingVariables
+{
+	public static void main( String[] args )
+	{
+		int room_num = 113;
+		double e = 2.71828;
+		String class_subject = "Computer Science";
+		
+		System.out.println( "This is room # " + room_num );
+		System.out.println( "e is close to " + e );
+		System.out.println( "I am learning a bit about " + class_subject );
+	}
+}
